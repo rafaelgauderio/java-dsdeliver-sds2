beb9e769f8bce97002232082db673c7f4ae5dc56
Rafael De Luca <deluca1712@gmail.com>
2021-01-10 20:59:05 -0300

    methods PUT /order/{id}/delivered

--- backend/src/main/java/com/devsupeior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsupeior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsupeior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,23 +9,54 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsupeior.dsdeliver.dto.OrderDTO;
+import com.devsupeior.dsdeliver.dto.ProductDTO;
 import com.devsupeior.dsdeliver.entities.Order;
+import com.devsupeior.dsdeliver.entities.OrderStatus;
+import com.devsupeior.dsdeliver.entities.Product;
 import com.devsupeior.dsdeliver.repositiries.OrderRepository;
+import com.devsupeior.dsdeliver.repositiries.ProductRepository;
 
 @Service
 
 public class OrderService {
-	
+
 	@Autowired
-	private OrderRepository repository;	
-	
+	private OrderRepository repository;
+
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
-		
+
 		List<Order> list = repository.findOrderWithProducts();
-		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());	
-		
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+
 	}
 	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
+		
+
+	}
 
 }
